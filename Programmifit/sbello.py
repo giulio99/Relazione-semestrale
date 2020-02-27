8992851043129c360f2eeb47a7fa3514c44af026
blancabru <58997940+blancabru@users.noreply.github.com>
2020-02-27 21:57:00 +0100

    Update and rename fit R1 = R2.py to sbello.py

--- Programmifit/sbello.py
+++ Programmifit/sbello.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pylab as py 
 from scipy.optimize import curve_fit
 
-f, df, vin, dvin, vout, dvout=np.loadtxt('R1 = R2.txt', unpack=True)
+f, df, vin, dvin, vout, dvout=np.loadtxt('R2_maggiore2_R1.txt', unpack=True)
 
 g=vout/vin
 dg=np.sqrt(((1/vin)*dvout)**2+((vout*dvout)/(vin**2))**2)
@@ -12,7 +12,7 @@ dg=np.sqrt(((1/vin)*dvout)**2+((vout*dvout)/(vin**2))**2)
 c2=1e-7
 c1=1e-5
 r1=330
-r2=330
+r2=33000
 
 ff=np.logspace(-1,6,1000)
 ww=2*np.pi*ff
@@ -36,23 +36,15 @@ def funz(f, r1, r2,c1,c2):
     
 init=(329,329,1e-5,1e-7)
 
-pars, covm = curve_fit(funz,f , g, init, dg, absolute_sigma=False)
-w0=1/(2*np.pi*np.sqrt((pars[0]*pars[1]*pars[2]*pars[3])))
-#w0=np.sqrt(1/pars[4])
-print('w0= %f'%w0)
-#print('Parametri iniziali:\n', pars)
-#print('Matrice di covarianza:\n', covm)
-print('R1= %f +- %f ' %(pars[0],np.sqrt(covm[0][0])))
-print('R2= %f +- %f ' %(pars[1],np.sqrt(covm[1][1])))
-print('C1= %.10f +- %f ' %(pars[2],np.sqrt(covm[2][2])))
-print('C2= %.10f +- %f ' %(pars[3],np.sqrt(covm[3][3])))
+
 
 plt.figure(1)
-plt.title('Guadagno filtro R1=R2', fontsize=16)
+plt.title('Guadagno filtro R2>R1', fontsize=16)
 plt.xlabel('Frequenza  [Hz]')
 plt.ylabel('Guadagno [u.a.]')
-plt.loglog(ff,funz(ff,*pars), color='blue')
-plt.loglog(ff, mod, color='lime')
+
+plt.loglog(ff, mod, color='blue')
 plt.errorbar(f, g, dg, df, ls='', color='red', marker='.')
-plt.grid()
+plt.grid(which='both', axis='y')
+plt.grid(axis='x')
 plt.show()
